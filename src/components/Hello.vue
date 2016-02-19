29756c51ffa7accb7e49a49cbdd980f296b9da6e
cyio <icaner@qq.com>
2016-02-19 17:57:58 +0800

    add jade loader

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="header">
+    <header class="ui-header ui-header-positive ui-border-b">
+      <i class="ui-icon-return" onclick="history.back()"></i><h1>{{tab}}</h1><button class="ui-btn">回首页</button>
+    </header>
   </div>
 </template>
 
@@ -12,7 +14,7 @@ export default {
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'Hello World!'
+      tab: '选项卡'
     }
   }
 }
